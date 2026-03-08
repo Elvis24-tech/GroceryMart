03cde159e0164aee7dc5e4f947b5435781c71c3a
elvis <kariuki.elvis@student.moringaschool.com>
2026-03-08 20:09:56 +0300

    changed the styling for the navbar

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,13 +1,16 @@
-const CategoryCard = ({ title, image }) => {
+const CategoryCard = ({ title, image, onClick }) => {
     return (
-      <div className="bg-white p-4 rounded-lg shadow-md text-center hover:shadow-xl">
+      <div
+        onClick={() => onClick(title)}
+        className="bg-white p-4 rounded-lg shadow-md text-center hover:shadow-xl cursor-pointer"
+      >
   
         <img
           src={image}
-          className="h-24 mx-auto mb-3"
+          className="h-24 mx-auto mb-3 rounded"
         />
   
-        <h3 className="font-semibold">
+        <h3 className="font-semibold text-lg">
           {title}
         </h3>
   
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="bg-green-600 text-white px-8 py-4 flex justify-between items-center">
+    <nav className="bg-green-700 text-white px-8 py-4 flex justify-between items-center">
 
       <h1 className="text-2xl font-bold">
         GroceryMart
